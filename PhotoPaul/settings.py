38c96ebd005e6de7fa13c67f5e3fa275a27f102c
leovt <leonhard.vogt@gmx.ch>
2017-10-12 21:49:05 +0200

    hiding editing ui when not logged in

--- PhotoPaul/settings.py
+++ PhotoPaul/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL = '/blog/'
+LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
--- PhotoPaul/urls.py
+++ PhotoPaul/urls.py
@@ -21,8 +21,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/blog/'}, name='logout'),
-    url(r'^blog/', include('PhotoBlog.urls', namespace='PhotoBlog')),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'', include('PhotoBlog.urls', namespace='PhotoBlog')),
 ]
 
 if settings.DEBUG:

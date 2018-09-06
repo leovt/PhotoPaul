588891fc33126f8427ee85e1a0cc14164debb939
leovt <leonhard.vogt@gmx.ch>
2018-09-06 20:05:43 +0200

    local settings with local sqlite db

---
+++ config/settings/localdb.py
@@ -0,0 +1,16 @@
+from .local import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': r'C:\Users\leonh\workspace\PhotoPaul\db.sqlite3',
+    }
+}
+
+ALLOWED_HOSTS = ['localhost']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = r'C:\Users\leonh\workspace\PhotoPaul' + '\\'
+
+SECRET_KEY = '8nl+o)*dgfzo2v0z6@90zc2l9e&)-ig(a5!_64=)p=#n=@uk9p'
+

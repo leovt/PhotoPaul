45cf6d4033d56e22a84274607ab61aa1712070bb
leovt <leonhard.vogt@gmx.ch>
2018-09-08 18:21:33 +0200

    imprint on server but not locally

--- config/settings/common.py
+++ config/settings/common.py
@@ -46,7 +46,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'PhotoBlog.apps.PhotoblogConfig',
-#    'imprint',
+    'imprint',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- config/settings/localdb.py
+++ config/settings/localdb.py
@@ -13,4 +13,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = r'C:\Users\leonh\workspace\PhotoPaul' + '\\'
 
 SECRET_KEY = '8nl+o)*dgfzo2v0z6@90zc2l9e&)-ig(a5!_64=)p=#n=@uk9p'
-
+INSTALLED_APPS = tuple(x for x in INSTALLED_APPS if x != 'imprint')
